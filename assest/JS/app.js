8872c2d645af4467ce8fc0261ef2a458f1422d81
Shahrukh Shahid <engr.shahrukh786@gmail.com>
2024-10-30 12:19:27 +0500

    update Length Function

--- assest/JS/app.js
+++ assest/JS/app.js
@@ -1,10 +1,31 @@
 var string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%&()?/><._!';
 function generatePassword(){
     var randomPassword = '';
-    for(var i = 0; i < 9; i++){
-        randomPassword += string[Math.floor(Math.random() * string.length)];
-        // console.log(randomPassword)
+    var length = document.getElementById('passwordLength').innerHTML;
+    // console.log(length)
+    if(length <=0){
+        alert('Please! Enter at least slect some range of password')
     }
-    document.getElementById('password').value = (`${randomPassword}`);
-    console.log(randomPassword)
+    else{
+        for(var i = 0; i < length; i++){
+            randomPassword += string[Math.floor(Math.random() * string.length)];
+            // console.log(randomPassword)
+        }
+        document.getElementById('password').value = (`${randomPassword}`);
+        console.log(randomPassword)
+    }
+    
+};
+
+function copy(){
+    let copy = document.getElementById('password');
+    copy.select();
+    navigator.clipboard.writeText(copy.value);
+}
+
+function updateLength(){
+    let slider = document.getElementById('slider');
+    let updateLength = document.getElementById('passwordLength');
+    console.log(updateLength.textContent)
+    updateLength.textContent = slider.value
 }
